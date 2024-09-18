65715f44361170196b2ec4ca532ec1b1bede6929
ArthurMorais0 <arthur162016@gmail.com>
2024-09-18 14:35:01 -0300

    react

---
+++ react-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './Components/Button'
+import CounterButton from './Components/CounterButton'
+import Input from './Components/Input'
+function App() {
+  return (
+    <>
+      {/* <Button /> */}
+     {/* <CounterButton />*/}
+     <Input/>
+    </>
+  )
+}
+
+export default App
